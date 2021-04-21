27412049c92022e720802fccb46bf59e6443a44d
Letian <nitewlt@gmail.com>
2021-04-21 09:54:00 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
+# Author: Letian Wu
+# Written: April 2021
 import pandas as pd
 import json
+import pprint
 from pandas import DataFrame
 
-
-
+#Part 1
 def read_csv(csv_path: str) -> DataFrame:
-    revenue_df = pd.read_csv(csv_path)
-    '''column_mapping = {"Revenue (in millions)":"Revenue",
-                   "Profit (in millions)": "Profit (in millions)"
-                  }
-    revenue_df.rename(columns=column_mapping, inplace=True)'''
-    print("There are {} rows in the raw dataset".format(revenue_df.shape[0]))
-    return revenue_df
+    rev_df = pd.read_csv(csv_path)
+    print("There are {} rows in the raw dataset".format(rev_df.shape[0]))
+    return rev_df
 
-def remove_non_numerical(revenue_df: DataFrame) -> DataFrame:
-    revenue_df = revenue_df[pd.to_numeric(revenue_df['Profit (in millions)'], errors='coerce').notnull()].copy()
-    revenue_df['Profit (in millions)'] = pd.to_numeric(revenue_df['Profit (in millions)'])
-    revenue_df.drop(['Profit (in millions)'], axis=1, inplace=True)
-    revenue_df.reset_index(inplace=True)
-    revenue_df.drop(['index'], axis=1, inplace=True)
-    print("There are {} rows in the dataset".format(revenue_df.shape[0]))
-    return revenue_df
+def remove_non_numerical(rev_df: DataFrame) -> DataFrame:
+    rev_df = rev_df[pd.to_numeric(rev_df['Profit (in millions)'], errors='coerce').notnull()].copy()
+    rev_df['Profit (in millions)'] = pd.to_numeric(rev_df['Profit (in millions)'])
+    print("There are {} rows in the updated dataset".format(rev_df.shape[0]))
+    return rev_df
 
-def store_json(revenue_df: DataFrame) -> None:
-    revenue_df.to_json(path_or_buf="data2.json",orient="table")
+#Part 2
+def store_as_json(rev_df: DataFrame) -> None:
+    rev_df.to_json(path_or_buf="data2.json",orient="table")
+    #alternative:with open('data2.json', 'w') as out_file: json.dump(valid_data, out_file, indent=2)
 
-def sort_revenue_df(revenue_df: DataFrame) -> DataFrame:
-    revenue_df.sort_values(by=['Profit (in millions)'], inplace=True, ascending=False, ignore_index=True)
-    print("Top 20 rows with highest Profit  values")
-    print(revenue_df.head(20))
-    return revenue_df
+def sort_rev_df(rev_df: DataFrame) -> DataFrame:
+    rev_df.sort_values(by=['Profit (in millions)'], inplace=True, ascending=False, ignore_index=True)
+    print("Top 20 company with highest profit values")
+    print(rev_df.head(20))
+    return rev_df
 
+# main
 if __name__ == '__main__':
     data_path = "data.csv"
 
-    company_revenue_df = read_csv(data_path)
-    company_revenue_df = remove_non_numerical(company_revenue_df)
+    company_rev_df = read_csv(data_path)
+    pprint.pprint(company_rev_df, indent=1)
+    company_rev_df = remove_non_numerical(company_rev_df)
+    pprint.pprint(company_rev_df, indent=1)
 
-
-    store_json(company_revenue_df)
-    sorted_revenue_df = sort_revenue_df(company_revenue_df)
+    store_as_json(company_rev_df)
+    sort_rev_df(company_rev_df)
